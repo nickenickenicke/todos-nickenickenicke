3c1ec56bfa6c034d33baad8ab46a33b5abc9f7b3
Nicke Bergman <nickenickebergman@gmail.com>
2023-11-16 16:29:53 +0100

    Added basic HTML structure

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,11 @@ import "./../scss/style.scss";
 
 const task1 = new Task("Krydda köttbullen", "Med ketchup");
 const task2 = new Task("Samla pengar till tårta", "Inte princesstårta");
-const task3 = new Task ("Gör hot shots", "Koka kaffe, vispa grädde!");
-const task4 = new Task ("Åk pendeltåg", "Jag är en loser baby");
+const task3 = new Task("Gör hot shots", "Koka kaffe, vispa grädde!");
+const task4 = new Task("Åk pendeltåg", "Jag är en loser baby");
+const task5 = new Task("En utan description")
 
-const tasks = [task1, task2, task3, task4];
+const tasks = [task1, task2, task3, task4, task5];
 
 console.log(tasks);
 
