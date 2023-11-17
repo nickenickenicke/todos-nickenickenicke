9b148bed4c1cc3dc4e0336994d21b6745c1f33d6
Nicke Bergman <nickenickebergman@gmail.com>
2023-11-17 16:44:50 +0100

    Added sorting functionality

--- src/js/htmlHelpers.js
+++ src/js/htmlHelpers.js
@@ -10,33 +10,72 @@ export const listLoop = (tasks) => {
     list.innerHTML = "";
     //Create each list element
     tasks.forEach ((task, i) => {
+        //Create elements
         const listItem = document.createElement("li");
         const listTitle = document.createElement("h4");
         const listDescription = document.createElement("span");
-
+        const btnChange = document.createElement("button");
+        const btnMoveUp = document.createElement("button");
+        const btnMoveDown = document.createElement("button");
+        
+        //Edit elements
+        btnChange.type = "button";
+        //Change inner text depending on list type
+        if (tasks == tasksActive){
+            btnChange.innerText = "Avklarad";
+        };
+        if (tasks == tasksInactive){
+            btnChange.innerText = "Återuppta";
+        };
+        btnMoveUp.type = "button";
+        btnMoveUp.innerText = "↑";
+        btnMoveDown.type = "button";
+        btnMoveDown.innerText = "↓";
         listTitle.innerText = task.title;
         listDescription.innerText = task.description;
 
+        //Place elements
         listItem.appendChild(listTitle);
         listItem.appendChild(listDescription);
+        //Don't add redundant sorting buttons to first and last item
+        if (i > 0) {
+            listItem.appendChild(btnMoveUp);
+        };
+        if (i < tasks.length-1) {
+            listItem.appendChild(btnMoveDown);
+        };
+        listItem.appendChild(btnChange);
         list.appendChild(listItem);
 
         //Functionality to remove item from list, insert into opposite list and regenerate html
-        listItem.addEventListener("click", () => {
+        btnChange.addEventListener("click", () => {
             task.isActive = !task.isActive;
 
             if (tasks == tasksActive){
             tasks.splice(i, 1);
             tasksInactive.push(task);
             }
-
             if (tasks == tasksInactive){
             tasks.splice(i, 1);
             tasksActive.push(task);
             }
-            
+
             createListHtml();
         });
+
+        //Functionality to move item up one position in list
+        btnMoveUp.addEventListener("click", () => {
+            tasks.splice(i, 1);
+            tasks.splice(i-1, 0, task);
+            createListHtml();
+        })
+
+        //Functionality to move item up down position in list
+        btnMoveDown.addEventListener("click", () => {
+            tasks.splice(i, 1);
+            tasks.splice(i+1, 0, task);
+            createListHtml();
+        })
     });
 };
 
